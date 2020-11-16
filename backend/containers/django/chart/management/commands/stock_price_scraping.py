d1bbeb61731b049a8269112772ecfcdeb59eaf30
toshi <toshi@toshi.click>
2020-11-16 22:23:53 +0900

    ちょっとログ出力追加

--- backend/containers/django/chart/management/commands/stock_price_scraping.py
+++ backend/containers/django/chart/management/commands/stock_price_scraping.py
@@ -1,7 +1,12 @@
 from django.core.management.base import BaseCommand
 from ..get_price import *
+import logging
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         df_t = get_price_time_designation('20200620', '20200625', '3769.jp')
+        # ロガーインスタンスを取得
+        logger = logging.getLogger('django')
+        # エラーメッセージをログ出力
+        logger.info(df_t)
 
