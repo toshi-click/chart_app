930907550859e6621026380cad01872983e36656
toshi <toshi@toshi.click>
2020-12-01 23:03:48 +0900

    株価情報をApiで使えるようにシリアライズクラス作成

--- backend/containers/django/restapi/serializers.py
+++ backend/containers/django/restapi/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Snippet
 from django.contrib.auth.models import User
 
+from chart.models import RawPrices
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RawPrices
+        fields = ('code', 'date', 'open_price', 'close_price', 'high_price', 'low_price', 'volume', 'moving_averages5', 'moving_averages25', 'moving_averages75', 'moving_averages100', 'moving_averages200')
+
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
